148204068ad7d364105ff3ad0a50bdfb14f88d72
2bona <sayhitobona@gmail.com>
2022-09-08 06:52:52 +0100

    dd

--- src/views/thankyou.vue
+++ src/views/thankyou.vue
@@ -30,8 +30,8 @@
         <ShareNetwork
     network="whatsapp"
     :url="('https://timely-rugelach-6a42db.netlify.app/#/')"
-    :title="'Sign up for Metro Connect'"
-    :description="'Click the link to chill with your fellow young CEOs and Founders crushing it in Abuja.'"
+    :title="'Sign up for Metro Connect 🚀'"
+    :description="'Click the link to chill 😎 with your fellow young CEOs 👨‍💼👩‍💼 and Founders 💎 crushing it in Abuja 🇳🇬.'"
     hashtags="https://timely-rugelach-6a42db.netlify.app/#/"
           style=" text-decoration:none"
   >
