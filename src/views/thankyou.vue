e7fb3e49ad1af09365f2876b6c45dadce78616d2
2bona <sayhitobona@gmail.com>
2022-09-08 07:59:22 +0100

    dd

--- src/views/thankyou.vue
+++ src/views/thankyou.vue
@@ -4,16 +4,15 @@
 
   <v-container dark class="grey--text px-5 text-center">
     <v-layout >
-      <v-flex xs5 class="d-flex justify-center align-center" sm4>
-    <v-avatar max-width="300px" tile width="100%" height="150"><v-img src="https://cdn.shopify.com/s/files/1/0133/3795/2320/products/Lacoste-Sticker-thermocollant-blanc_1200x1200.jpg?v=1553936753"></v-img></v-avatar>
-
+      <v-flex xs5 class="d-flex justify-center align-center" sm5>
+    <v-avatar max-width="490px" tile width="100%" height="120"><v-img contain src="https://cdn.shopify.com/s/files/1/0133/3795/2320/products/Lacoste-Sticker-thermocollant-blanc_1200x1200.jpg?v=1553936753"></v-img></v-avatar>
       </v-flex>
-      <v-flex xs2 class="d-flex justify-center align-center" sm4>
+      <v-flex xs2 class="d-flex justify-center align-center" sm2>
       <v-icon size color="white">mdi-arrow-projectile-multiple</v-icon>
       
       </v-flex>
-      <v-flex xs5 class="d-flex justify-center align-center" sm4>
-    <v-avatar class="mt-3" max-width="200px" tile width="100%" height="auto"><v-img src="https://cdn.freebiesupply.com/images/large/2x/google-logo-transparent.png"></v-img></v-avatar>
+      <v-flex xs5 class="d-flex justify-center align-center" sm5>
+    <v-avatar class="" max-width="180px" tile width="100%" height="auto"><v-img contain src="https://cdn.freebiesupply.com/images/large/2x/google-logo-transparent.png"></v-img></v-avatar>
       </v-flex>
     </v-layout>
 
